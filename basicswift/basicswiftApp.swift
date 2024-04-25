1d9b9aa746adfa3b2afe00c987dcc94dd5bc2d27
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-04-25 09:23:57 +0530

    Initial Commit

---
+++ basicswift/basicswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicswiftApp.swift
+//  basicswift
+//
+//  Created by shashwat singh on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct basicswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
