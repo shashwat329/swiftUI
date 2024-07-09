2802f1446b46d359b572f67a353b8150a0d52ca2
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-07-09 23:56:36 +0530

    subview and blinding property

---
+++ basicswift/BindingView.swift
@@ -0,0 +1,54 @@
+//
+//  BindingView.swift
+//  basicswift
+//
+//  Created by shashwat singh on 09/07/24.
+//
+
+import SwiftUI
+
+struct BindingView: View {
+    
+    @State var backgroundColor: Color = Color.green
+    @State var title: String = "Title"
+    
+    var body: some View {
+        ZStack {
+            backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text(title)
+                    .foregroundColor(.white)
+                
+                ButtonView(backgroundColor: $backgroundColor, title: $title)
+            }
+        }
+    }
+}
+
+struct ButtonView: View {
+    
+    @Binding var backgroundColor: Color
+    @State var buttonColor: Color = Color.blue
+    @Binding var title: String
+    
+    var body: some View {
+        Button(action: {
+            backgroundColor = Color.orange
+            buttonColor = Color.pink
+            title = "NEW TITLE!!!!!!!"
+        }, label: {
+            Text("Button")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(buttonColor)
+                .cornerRadius(10)
+        })
+    }
+}
+
+#Preview {
+    ButtonView(backgroundColor: Color.green, title: "updated")
+}
