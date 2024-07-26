68ec13cffe38f9d3c44ce750eaaa86ee79dfb60e
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-07-26 23:56:06 +0530

    list

---
+++ basicswift/listbootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  listbootcamp.swift
+//  basicswift
+//
+//  Created by shashwat singh on 26/07/24.
+//
+
+import SwiftUI
+
+struct listbootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listbootcamp()
+}
