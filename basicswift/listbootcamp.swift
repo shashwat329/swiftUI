6db4c2fd41030a091c6bc53d85e8b84a8d600b60
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-07-27 23:33:39 +0530

    list view

--- basicswift/listbootcamp.swift
+++ basicswift/listbootcamp.swift
@@ -7,12 +7,66 @@
 
 import SwiftUI
 
-struct listbootcamp: View {
+struct ListBootcamp: View {
+    @State private var fruits: [String] = ["apple", "banana", "mango", "guava"]
+    @State private var veggies: [String] = ["potato", "tomato", "green veg", "romes"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section(
+                    header: Text("Fruits")) {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit.capitalized)
+                    }
+                    .onDelete(perform: deleteFruit)
+                    .onMove(perform: { indices , newOffset in
+                        fruits.move(fromOffsets: indices, toOffset: newOffset)
+                    })
+                }
+                Section(header: Text("Veggies")) {
+                    ForEach(veggies, id: \.self) { veggie in
+                        Text(veggie.capitalized)
+                    }
+                    .onDelete(perform: deleteVeggie)
+                    .onMove(perform: moveVeggie)
+                }
+            }
+          
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("List")
+            .navigationBarItems(
+                leading: EditButton(),
+                trailing: addbuttonView
+            )
+        }
+        .accentColor(.blue)
+    }
+    var addbuttonView: some View{
+        Button("add", action: {
+            add()
+        })
+    }
+    func deleteFruit(at offsets: IndexSet) {
+        fruits.remove(atOffsets: offsets)
+    }
+    
+    func moveFruit(from source: IndexSet, to destination: Int) {
+        fruits.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func add(){
+        fruits.append("sgasgw")
+    }
+    func deleteVeggie(at offsets: IndexSet) {
+        veggies.remove(atOffsets: offsets)
+    }
+    
+    func moveVeggie(from source: IndexSet, to destination: Int) {
+        veggies.move(fromOffsets: source, toOffset: destination)
     }
 }
 
 #Preview {
-    listbootcamp()
+    ListBootcamp()
 }
